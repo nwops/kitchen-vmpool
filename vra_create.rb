5701e80fddd2b4fc1442ac73762533428c3ba243
Corey Osman <corey@nwops.io>
2017-08-30 16:35:08 -0400

    update scripts to work with vra7

--- vra_create.rb
+++ vra_create.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 require 'vra'
+require 'fileutils'
 require 'erb'
 require 'highline/import'
 require 'openssl'
@@ -7,35 +8,81 @@ require 'json'
 require 'yaml'
 
 # Purpose: Submits a single request to VRA for vm creation
+
+
+# monkey patch strings to symbols until we can patch upstream
 module Vra
-  class Client
-    # monkey patch the init method to accept token
-    def initialize(opts)
-      @base_url     = opts[:base_url]
-      @username     = opts[:username]
-      @password     = PasswordMasker.new(opts[:password])
-      @tenant       = opts[:tenant]
-      @verify_ssl   = opts.fetch(:verify_ssl, true)
-      @bearer_token = PasswordMasker.new(nil)
-      @page_size    = opts.fetch(:page_size, 20)
-
-      validate_client_options!
-    end
-  end
-
-  # monkey patch the init method to accept additional params
   class CatalogRequest
-    def initialize(client, catalog_id, opts = {})
-      @client            = client
-      @catalog_id        = catalog_id
-      @cpus              = opts[:cpus]
-      @memory            = opts[:memory]
-      @requested_for     = opts[:requested_for]
-      @lease_days        = opts[:lease_days]
-      @notes             = opts[:notes]
-      @subtenant_id      = opts[:subtenant_id]
-      @additional_params = opts[:additional_params] || Vra::RequestParameters.new
-      @catalog_item = Vra::CatalogItem.new(client, id: catalog_id)
+    attr_accessor :template_payload
+
+    def template_payload
+      @template_payload ||= dump_template(@catalog_id) 
+    end
+
+    def template_payload=(payload)
+      @template_payload = payload
+    end
+
+    def write_template(id, filename = nil)
+      filename ||= "#{id}.json" 
+      begin
+        puts "Writing file #{filename}"
+        contents = dump_template(id)
+        data = JSON.parse(contents)
+        pretty_contents = JSON.pretty_generate(data)
+        File.write(filename, pretty_contents)
+      rescue Vra::Exception::HTTPError => e
+        puts e.message
+      end
+    end
+
+    def dump_template(id)
+      response = client.http_get("/catalog-service/api/consumer/entitledCatalogItems/#{id}/requests/template")
+      response.body
+    end
+
+    def merged_payload
+       merge_payload(template_payload)
+    end
+
+    def submit
+      validate_params!
+
+      begin
+        post_response = client.http_post("/catalog-service/api/consumer/entitledCatalogItems/#{@catalog_id}/requests", merged_payload)
+      rescue Vra::Exception::HTTPError => e
+        raise Vra::Exception::RequestError, "Unable to submit request: #{e.errors.join(', ')}"
+      rescue
+        raise
+      end
+
+      request_id = JSON.parse(post_response.body)["id"]
+      Vra::Request.new(client, request_id)
+    end
+
+  end
+  class RequestParameters
+    def set_parameters(key, value_data, parent = nil)
+      value_type = value_data[:type] || value_data['type']
+      data_value = value_data[:value] || value_data['value']
+      if value_type
+        if parent.nil?
+          set(key, value_type, data_value)
+        else
+          parent.add_child(Vra::RequestParameter.new(key, value_type, data_value))
+        end
+      else
+        if parent.nil?
+          p = set(key, nil, nil)
+        else
+          p = Vra::RequestParameter.new(key, nil, nil)
+          parent.add_child(p)
+        end
+
+        value_data.each do |k, data|
+          set_parameters(k, data, p)
+        end
+      end
     end
   end
 end
@@ -54,7 +101,7 @@ module VraUtilities
   end
 
   def datacenter
-    ENV['DATACENTER'] || 'Eroc'
+    ENV['DATACENTER']
   end
 
   def vra_email
@@ -77,18 +124,6 @@ module VraUtilities
     @server ||= ENV['VRA_URL']
   end
 
-  # @return [Hash] - the the hash value of the key_name found in the request data payload
-  def data_item(key_name)
-    request_data["requestData"]['entries'].find {|e| e["key"] == key_name }
-  end
-
-  # @return [String] - the value of the key_name found in the request data payload
-  def data_item_value(key_name)
-    item = data_item(key_name)
-    return item if item.nil?
-    item["value"]["value"]
-  end
-
   # @return [VRA::Client] - creates a new client object and returns it
   def client
     @client ||= Vra::Client.new(
@@ -105,46 +140,50 @@ module VraUtilities
    client.catalog.all_items.map {|i| {name: i.name, id: i.id}}
   end
 
+  def template_data
+    @template_data ||= JSON.parse(File.read(@payload_file))
+  end
+
   def request_options
     {
-      cpus: data_item_value("provider-VirtualMachine.CPU.Count") || 1,
-      memory: data_item_value("provider-VirtualMachine.Memory.Size") || 4096,
-      requested_for: ENV['VRA_USER'] || data_item_value("requestedFor"),
+      cpus: template_data['data']['Machine']['data']['cpu'] || 2,
+      memory: template_data['data']['Machine']['data']['memory'] || 4096,
+      requested_for: ENV['VRA_USER'] 
       lease_days: 2,
-      additional_params: request_params,
       notes: 'VRA Server Pool Test',
-      subtenant_id: request_data['organization']['subtenantRef']
+      subtenant_id: template_data['businessGroupId']
     }
   end
 
-  def request_data
-    @request_data ||= YAML.load_file(@payload_file)
-  end
-
-  def parameters
-    request_data['requestData']['entries'].map {|item| [item['key'], item['value'].values].flatten }
-  end
-
-  def request_params
-    unless @request_params
-      @request_params = Vra::RequestParameters.new
-      parameters.each { |p| @request_params.set(*p)}
-    end
-    @request_params
-  end
-
-  def request_item
-     blueprint = request_data['catalogItemRef']['id']
-     client.catalog.request(blueprint, request_options)
+  def catalog_request 
+     blueprint = template_data['catalogItemId']
+     cr = client.catalog.request(blueprint, request_options)
+     cr.template_payload = File.read(@payload_file)
+     cr
   end
 
   # @return [Vra::Request] - returns a request item
-  def submit_new_request(payload_file)
-    @payload_file = payload_file
-    request_item.submit
+  def submit_new_request(file)
+    @payload_file = File.expand_path(file)
+    unless @payload_file and File.exist?(@payload_file)
+      puts "The payload file: #{@payload_file} does not exist"
+      exit -1 
+    end
+    cr = catalog_request 
+    cr.submit
   end
 
   require 'optparse'
+
+  def dump_templates(dir_name = 'vra7_templates')
+    FileUtils.mkdir(dir_name) unless File.exist?(dir_name)
+    catalog_items.each do |c|
+      cr = catalog_request
+      filename = File.join(dir_name, "#{c[:name]}.json".gsub(' ', '_')).downcase
+      cr.write_template(c[:id], filename)
+    end
+  end
+
 
   def run
   	cli_options = {}
@@ -158,9 +197,13 @@ module VraUtilities
   	  opts.on('-n', '--node-file FILE', "Load the request data from this file and create it") do |c|
   	    cli_options[:node_file] = c
   	  end
-  	end.parse!
-   submit_new_request(cli_options[:node_file]) if File.exist?(cli_options[:node_file]) # create the request
+          opts.on('-t', '--dump-templates', "Dump all catalog templates") do |c|
+            cli_options[:dump_templates] = true
+          end
+        end.parse!
+   @payload_file = cli_options[:node_file]
+   dump_templates if cli_options[:dump_templates]
+   submit_new_request(@payload_file) 
   end
 end
-
 include VraUtilities
